599d45590c90aae4bbcae769a7f1ab29ae573f3a
Shubham Kumar <34469341+PIR4T3@users.noreply.github.com>
2018-03-22 03:39:53 +0000

    Add files via upload
    lucas.c

---
+++ General/lucas.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(int a, char* p[]){
+   int first = 2, second = 1;
+   int sum = 0, limit, i;
+   limit = atoi(p[1]);
+   for(i=0; i<limit; i++){
+      printf("%d ",first);
+      sum = first + second;
+      first = second;
+      second = sum;
+   }
+   
+}
